5be0af2eb98cbc456301ea5df0587aa53a338137
Linus Ehlert <C:/Program Files/Git/linus.ehlert@outlook.de>
2022-04-19 12:27:35 +0800

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -5,7 +5,27 @@ const path = require("path");
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req,res) => {
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/rotten-potatoes');
+
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String
+});
+
+app.get('/', (req, res) => {
+    Review.find()
+        .then(reviews => {
+            res.render('reviews-index', {reviews: reviews});
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+
+
+app.get('/', (req, res) => {
     res.render('/home', {msg: 'Moin'})
 })
 
